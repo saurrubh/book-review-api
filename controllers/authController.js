483f86eff185bfaad66d9b73ca1075fa9ef1c800
Saurabh Singh <s.singh@bm-systems.in>
2025-05-23 18:53:52 +0530

    fixed working for api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,28 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const generateToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 };
 
-exports.signup = (req, res) => {
+exports.signup = async (req, res) => {
   const { username, password } = req.body;
-  res.status(201).json({ message: `User ${username} signed up.` });
+  try {
+    const user = await User.create({ username, password });
+    res.status(201).json({ message: "User created" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
-
 
 exports.login = async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await User.findOne({ username });
     if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      return res.status(401).json({ error: "Invalid credentials" });
     }
     const token = generateToken(user);
     res.json({ token });
